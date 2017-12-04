1d650d44576e4f2b2a69670ff6e538a32b1d1aa6
abdulbasheer <abdulbsr24@gmail.com>
2017-12-04 20:09:10 +0530

    propeties add

--- Login2/Class1.cs
+++ Login2/Class1.cs
@@ -7,5 +7,10 @@ namespace Login2
 {
     public class Class1
     {
+
+        public string name { get; set; }
+        public string add { get; set; }
+        public int age { get; set; }
+        public string gender { get; set; }
     }
 }
